f95e99657fe7078c8711bee8e71aa13a34c8eabe
cherelemma <chere.lemma@aastu.edu.et>
2023-07-28 01:32:16 +0300

    debug and fix rubocop linters

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,11 +1,11 @@
-require "test_helper"
+require 'test_helper'
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get home_index_url
     assert_response :success
 
     # Check for server timing header
-    assert_equal "app;dur=1.0", response.headers["Server-Timing"]
+    assert_equal 'app;dur=1.0', response.headers['Server-Timing']
   end
 end
