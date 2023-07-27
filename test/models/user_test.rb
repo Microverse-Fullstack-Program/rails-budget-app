a47e5238ebd48bb767e6ebedb626fa7a387e4f50
cherelemma <chere.lemma@aastu.edu.et>
2023-07-28 00:39:37 +0300

    fix another linter errors

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,38 +1,38 @@
-require "test_helper"
+require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  test "should not save user without name" do
+  test 'should not save user without name' do
     user = User.new
     assert_not user.save
   end
 
-  test "should not save user with name less than 3 characters" do
-    user = User.new(name: "ab")
+  test 'should not save user with name less than 3 characters' do
+    user = User.new(name: 'ab')
     assert_not user.save
   end
 
-  test "should not save user with name greater than 50 characters" do
-    user = User.new(name: "a" * 51)
+  test 'should not save user with name greater than 50 characters' do
+    user = User.new(name: 'a' * 51)
     assert_not user.save
   end
 
-  test "should save user with name greater than 3 characters" do
-    user = User.new(name: "abc")
+  test 'should save user with name greater than 3 characters' do
+    user = User.new(name: 'user')
     assert user.save
   end
 
-  test "should not save user without email" do
-    user = User.new(name: "abc")
+  test 'should not save user without email' do
+    user = User.new(name: 'user')
     assert_not user.save
   end
 
-  test "should not save user with invalid email" do
-    user = User.new(name: "abc", email: "abc")
+  test 'should not save user with invalid email' do
+    user = User.new(name: 'user', email: 'user')
     assert_not user.save
   end
 
-  test "should save user with valid email" do
-    user = User.new(name: "abc", email: "admin@localhost"
+  test 'should save user with valid email' do
+    user = User.new(name: 'user', email: 'admin@localhost')
     assert user.save
   end
 end
