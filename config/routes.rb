9813cec80c48c7b4c3b5e9f5194948dd24645334
cherelemma <chere.lemma@aastu.edu.et>
2023-07-29 06:52:57 +0300

    add routes for categories and entities

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root "home#index"
 
+  resources :categories do
+    resources :entities
+  end
+
+  devise_scope :user do
+    get 'logout', to: 'devise/sessions#destroy'
+  end
+
 end
